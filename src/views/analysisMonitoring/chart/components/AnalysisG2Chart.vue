f9b4429be618673ac77f5c50c6e3b7637c89e5d7
emozhu <2933351711@qq.com>
2026-04-24 16:47:13 +0800

    commit

--- src/views/analysisMonitoring/chart/components/AnalysisG2Chart.vue
+++ src/views/analysisMonitoring/chart/components/AnalysisG2Chart.vue
@@ -1,5 +1,7 @@
 <template>
-  <div ref="chartRef" class="analysis-g2-chart" />
+  <n-spin :show="rendering" class="analysis-g2-chart-spin">
+    <div ref="chartRef" class="analysis-g2-chart" />
+  </n-spin>
 </template>
 
 <script lang="ts" setup>
@@ -12,6 +14,7 @@ interface ChartDatum {
   count: number
   group?: string
   label?: string
+  tooltip?: string
 }
 
 const props = withDefaults(
@@ -29,7 +32,18 @@ const props = withDefaults(
 
 const palette = ['#78a6f6', '#72cfae', '#f8bd67', '#a98af0', '#ef7bb8', '#f17777', '#8c8c8c']
 const chartRef = ref<HTMLElement | null>(null)
+const rendering = ref(false)
 let chart: G2Chart | null = null
+
+const axisStyle = {
+  title: false,
+  labelFill: '#000000',
+  labelOpacity: 1,
+  labelFontSize: 12,
+  labelFontWeight: 400,
+  line: true,
+  tick: true
+}
 
 const destroyChart = () => {
   chart?.destroy()
@@ -38,12 +52,13 @@ const destroyChart = () => {
 
 const createBaseChart = () => {
   if (!chartRef.value) return null
+  const padding = props.type === 'donut' ? ([36, 96, 36, 96] as [number, number, number, number]) : 'auto'
 
   return new G2Chart({
     container: chartRef.value,
     autoFit: true,
-    height: 350,
-    padding: 'auto'
+    height: 300,
+    padding: padding as never
   })
 }
 
@@ -52,10 +67,10 @@ const setLegend = (instance: G2Chart) => {
     instance.legend('color', {
       position: 'top',
       layout: { justifyContent: 'center' },
-      itemLabelFontSize: 16,
-      itemLabelFontWeight: 600,
-      itemMarkerSize: 14,
-      itemSpacing: 22
+      itemLabelFontSize: 14,
+      itemLabelFontWeight: 400,
+      itemMarkerSize: 11,
+      itemSpacing: 18
     })
     return
   }
@@ -76,25 +91,27 @@ const renderBarChart = (instance: G2Chart) => {
     .scale('color', { range: palette })
     .style('radiusTopLeft', 3)
     .style('radiusTopRight', 3)
-    .tooltip((data: ChartDatum) => ({ name: data.item, value: data.count }))
+    .tooltip((data: ChartDatum) => ({ name: data.tooltip || data.item, value: data.count }))
 
   if (props.showLabel) {
     interval.label({
-      text: (data: ChartDatum) => data.label || `${data.count}`,
-      position: 'inside',
-      fill: '#fff',
+      text: (data: ChartDatum) => `${data.count}`,
+      position: 'right',
+      fill: '#000000',
       fontSize: 12,
+      fontWeight: 400,
+      dx: 8,
       transform: [{ type: 'overlapDodgeY' }]
     })
   }
 
-  instance.axis('x', { title: false, labelAutoHide: true, labelFill: '#606266' })
-  instance.axis('y', { title: false, grid: true, gridLineDash: [4, 4], labelFill: '#909399' })
+  instance.axis('x', { ...axisStyle, labelAutoHide: true })
+  instance.axis('y', { ...axisStyle, grid: true, gridLineDash: [4, 4] })
   setLegend(instance)
 }
 
 const renderRingChart = (instance: G2Chart) => {
-  instance.coordinate({ type: 'theta', outerRadius: 0.78, innerRadius: 0.56 })
+  instance.coordinate({ type: 'theta', outerRadius: 0.68, innerRadius: 0.5 })
   const interval = instance
     .interval()
     .data(props.data)
@@ -110,7 +127,9 @@ const renderRingChart = (instance: G2Chart) => {
     interval.label({
       position: 'outside',
       text: (data: ChartDatum) => `${data.item}：${data.count}`,
-      fontSize: 12
+      fontSize: 12,
+      fill: '#000000',
+      connector: true
     })
   }
 
@@ -150,22 +169,27 @@ const renderLineChart = (instance: G2Chart) => {
     .tooltip(false)
 
   instance.scale('y', { domainMin: 0, nice: true })
-  instance.axis('x', { title: false, labelAutoRotate: false, labelFill: '#606266' })
-  instance.axis('y', { title: false, grid: true, gridLineDash: [4, 4], labelFill: '#909399' })
+  instance.axis('x', { ...axisStyle, labelAutoRotate: false })
+  instance.axis('y', { ...axisStyle, grid: true, gridLineDash: [4, 4] })
   setLegend(instance)
 }
 
 const renderChart = async () => {
+  rendering.value = true
   await nextTick()
-  destroyChart()
-  const instance = createBaseChart()
-  if (!instance) return
+  try {
+    destroyChart()
+    const instance = createBaseChart()
+    if (!instance) return
 
-  chart = instance
-  if (props.type === 'bar') renderBarChart(instance)
-  if (props.type === 'donut') renderRingChart(instance)
-  if (props.type === 'line') renderLineChart(instance)
-  instance.render()
+    chart = instance
+    if (props.type === 'bar') renderBarChart(instance)
+    if (props.type === 'donut') renderRingChart(instance)
+    if (props.type === 'line') renderLineChart(instance)
+    await Promise.resolve(instance.render())
+  } finally {
+    rendering.value = false
+  }
 }
 
 const downloadImage = async (fileName: string) => {
@@ -189,8 +213,16 @@ defineExpose({ downloadImage })
 </script>
 
 <style scoped lang="scss">
+.analysis-g2-chart-spin {
+  width: 100%;
+}
+
 .analysis-g2-chart {
   width: 100%;
-  height: 350px;
+  height: 300px;
+}
+
+.analysis-g2-chart-spin :deep(.n-spin-content) {
+  width: 100%;
 }
 </style>
--- src/views/analysisMonitoring/chart/components/ChartCard.vue
+++ src/views/analysisMonitoring/chart/components/ChartCard.vue
@@ -1,13 +1,10 @@
 <template>
-  <div v-if="hasVisibleData" ref="cardRef" class="chart-card">
+  <div v-if="hasVisibleData" class="chart-card">
     <div class="chart-card__header">
       <div class="chart-card__title">{{ card.title }}</div>
     </div>
 
-    <div
-      class="chart-card__body"
-      :class="{ 'chart-card__body--rank': showRankPanel }"
-    >
+    <div class="chart-card__body" :class="{ 'chart-card__body--rank': showRankPanel }">
       <div class="chart-card__chart-wrap">
         <AnalysisG2Chart
           ref="chartRef"
@@ -18,11 +15,7 @@
         />
       </div>
       <div v-if="showRankPanel" class="chart-rank">
-        <div
-          v-for="(item, index) in rankItems"
-          :key="item.name"
-          class="chart-rank__item"
-        >
+        <div v-for="(item, index) in rankItems" :key="item.name" class="chart-rank__item">
           <span class="chart-rank__badge">NO.{{ index + 1 }}</span>
           <div class="chart-rank__content">
             <span class="chart-rank__name">{{ item.name }}分布</span>
@@ -32,135 +25,133 @@
       </div>
     </div>
 
-    <div
-      v-if="hasAction('setting') || hasAction('download')"
-      class="chart-card__tools"
-    >
-      <n-popover
-        v-if="hasAction('setting')"
-        trigger="click"
-        placement="left-start"
-      >
-        <template #trigger>
-          <button class="chart-card__tool" type="button" title="设置">
-            <svg viewBox="0 0 24 24" aria-hidden="true">
-              <path
-                d="M19.43 12.98c.04-.32.07-.65.07-.98s-.02-.66-.07-.98l2.11-1.65a.5.5 0 0 0 .12-.64l-2-3.46a.5.5 0 0 0-.6-.22l-2.49 1a7.28 7.28 0 0 0-1.69-.98L14.5 2.42A.49.49 0 0 0 14 2h-4a.49.49 0 0 0-.49.42l-.38 2.65c-.61.24-1.18.56-1.69.98l-2.49-1a.5.5 0 0 0-.6.22l-2 3.46a.5.5 0 0 0 .12.64l2.11 1.65c-.04.32-.08.65-.08.98s.03.66.08.98l-2.11 1.65a.5.5 0 0 0-.12.64l2 3.46c.13.23.4.32.6.22l2.49-1c.51.4 1.08.73 1.69.98l.38 2.65c.04.24.25.42.49.42h4c.24 0 .45-.18.49-.42l.38-2.65c.61-.24 1.18-.56 1.69-.98l2.49 1c.23.08.48 0 .6-.22l2-3.46a.5.5 0 0 0-.12-.64l-2.11-1.65ZM12 15.5A3.5 3.5 0 1 1 12 8a3.5 3.5 0 0 1 0 7.5Z"
-              />
-            </svg>
-          </button>
-        </template>
-        <div class="chart-setting">
-          <div class="chart-setting__title">图表设置</div>
-          <n-space vertical :size="12">
-            <n-switch v-model:value="chartSetting.showLegend">
-              <template #checked>显示图例</template>
-              <template #unchecked>隐藏图例</template>
-            </n-switch>
-            <n-switch v-model:value="chartSetting.showLabel">
-              <template #checked>显示数值</template>
-              <template #unchecked>隐藏数值</template>
-            </n-switch>
-          </n-space>
-        </div>
-      </n-popover>
-      <button
-        v-if="hasAction('download')"
-        class="chart-card__tool"
-        type="button"
-        title="下载"
-        @click="onDownload"
-      >
+    <div v-if="hasAction('setting') || hasAction('download')" class="chart-card__tools">
+      <button v-if="hasAction('setting')" class="chart-card__tool" type="button" title="设置" @click="openSetting">
         <svg viewBox="0 0 24 24" aria-hidden="true">
-          <path
-            d="M12 3a1 1 0 0 1 1 1v9.17l3.59-3.58A1 1 0 1 1 18 11l-5.3 5.3a1 1 0 0 1-1.4 0L6 11a1 1 0 1 1 1.41-1.41L11 13.17V4a1 1 0 0 1 1-1Zm-7 15a1 1 0 0 1 1 1v1h12v-1a1 1 0 1 1 2 0v2a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1Z"
-          />
+          <path d="M19.43 12.98c.04-.32.07-.65.07-.98s-.02-.66-.07-.98l2.11-1.65a.5.5 0 0 0 .12-.64l-2-3.46a.5.5 0 0 0-.6-.22l-2.49 1a7.28 7.28 0 0 0-1.69-.98L14.5 2.42A.49.49 0 0 0 14 2h-4a.49.49 0 0 0-.49.42l-.38 2.65c-.61.24-1.18.56-1.69.98l-2.49-1a.5.5 0 0 0-.6.22l-2 3.46a.5.5 0 0 0 .12.64l2.11 1.65c-.04.32-.08.65-.08.98s.03.66.08.98l-2.11 1.65a.5.5 0 0 0-.12.64l2 3.46c.13.23.4.32.6.22l2.49-1c.51.4 1.08.73 1.69.98l.38 2.65c.04.24.25.42.49.42h4c.24 0 .45-.18.49-.42l.38-2.65c.61-.24 1.18-.56 1.69-.98l2.49 1c.23.08.48 0 .6-.22l2-3.46a.5.5 0 0 0-.12-.64l-2.11-1.65ZM12 15.5A3.5 3.5 0 1 1 12 8a3.5 3.5 0 0 1 0 7.5Z" />
+        </svg>
+      </button>
+      <button v-if="hasAction('download')" class="chart-card__tool" type="button" title="下载" @click="onDownload">
+        <svg viewBox="0 0 24 24" aria-hidden="true">
+          <path d="M12 3a1 1 0 0 1 1 1v9.17l3.59-3.58A1 1 0 1 1 18 11l-5.3 5.3a1 1 0 0 1-1.4 0L6 11a1 1 0 1 1 1.41-1.41L11 13.17V4a1 1 0 0 1 1-1Zm-7 15a1 1 0 0 1 1 1v1h12v-1a1 1 0 1 1 2 0v2a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1Z" />
         </svg>
       </button>
     </div>
+
+    <n-modal v-model:show="settingVisible" preset="card" title="参数设置" class="chart-setting-modal">
+      <n-form label-placement="left" label-width="72" :model="settingForm" class="chart-setting-form">
+        <n-form-item label="名次：">
+          <n-input-number v-model:value="settingForm.rankCount" :min="1" :show-button="false" class="chart-setting-input" />
+        </n-form-item>
+        <n-form-item label="类型：">
+          <n-select v-model:value="settingForm.type" :options="typeOptions" class="chart-setting-input" />
+        </n-form-item>
+        <n-form-item label="年份：">
+          <n-select v-model:value="settingForm.year" :options="yearOptions" class="chart-setting-input" />
+        </n-form-item>
+        <n-form-item label="月份：">
+          <n-select v-model:value="settingForm.month" :options="monthOptions" clearable placeholder="请选择" class="chart-setting-input" />
+        </n-form-item>
+      </n-form>
+      <template #footer>
+        <n-space justify="end">
+          <j-button round @click="settingVisible = false">取消</j-button>
+          <j-button type="info" round @click="confirmSetting">确定</j-button>
+        </n-space>
+      </template>
+    </n-modal>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, reactive, ref } from "vue";
-import type { AnalysisChartCard } from "../src/types/chart";
-import AnalysisG2Chart from "./AnalysisG2Chart.vue";
+import { computed, reactive, ref } from 'vue'
+import { JButton } from 'junegoo-ui'
+import type { AnalysisChartCard } from '../src/types/chart'
+import AnalysisG2Chart from './AnalysisG2Chart.vue'
 
-type ChartInstance = InstanceType<typeof AnalysisG2Chart> | null;
+type ChartInstance = InstanceType<typeof AnalysisG2Chart> | null
 
 const props = defineProps<{
-  card: AnalysisChartCard;
-}>();
+  card: AnalysisChartCard
+}>()
 
-const chartRef = ref<ChartInstance>(null);
+const chartRef = ref<ChartInstance>(null)
+const settingVisible = ref(false)
 const chartSetting = reactive({
   showLegend: true,
-  showLabel: true,
-});
+  showLabel: true
+})
+const settingForm = reactive({
+  rankCount: 10,
+  type: null as string | null,
+  year: null as string | null,
+  month: null as string | null
+})
 
-const hasAction = (type: "setting" | "download") =>
-  props.card.actions.some((action) => action.type === type);
+const typeOptions = [
+  { label: '加分', value: 'plus' },
+  { label: '减分', value: 'minus' }
+]
+const yearOptions = [
+  { label: '2026年', value: '2026' },
+  { label: '2025年', value: '2025' },
+  { label: '2024年', value: '2024' }
+]
+const monthOptions = Array.from({ length: 12 }, (_, index) => ({
+  label: `${index + 1}月`,
+  value: String(index + 1)
+}))
+
+const hasAction = (type: 'setting' | 'download') =>
+  props.card.actions?.some((action) => action.type === type)
 
 const rankItems = computed(() =>
-  [...(props.card.donut || [])]
-    .sort((prev, next) => next.value - prev.value)
-    .slice(0, 2),
-);
+  [...(props.card.donut || [])].sort((prev, next) => next.value - prev.value).slice(0, 2)
+)
 
-const showRankPanel = computed(
-  () => props.card.kind === "donut" && rankItems.value.length > 0,
-);
+const showRankPanel = computed(() => props.card.kind === 'donut' && rankItems.value.length > 0)
 
 const hasVisibleData = computed(() => {
-  if (props.card.kind === "bar")
-    return Boolean(props.card.bars?.some((item) => item.value !== 0));
-  if (props.card.kind === "donut")
-    return Boolean(props.card.donut?.some((item) => item.value !== 0));
-  if (props.card.kind === "line") {
-    return Boolean(
-      props.card.lines?.some((line) =>
-        line.values.some((value) => value !== 0),
-      ),
-    );
-  }
-  return false;
-});
+  if (props.card.kind === 'bar') return Boolean(props.card.bars?.some((item) => item.value !== 0))
+  if (props.card.kind === 'donut') return Boolean(props.card.donut?.some((item) => item.value !== 0))
+  return Boolean(props.card.lines?.some((line) => line.values.some((value) => value !== 0)))
+})
 
 const chartData = computed(() => {
-  if (props.card.kind === "bar") {
-    return (props.card.bars || []).map((item) => ({
-      item: item.name,
-      count: item.value,
-      label: item.label,
-    }));
+  if (props.card.kind === 'bar') {
+    return (props.card.bars || []).map((item) => ({ item: item.name, count: item.value, tooltip: item.label }))
   }
 
-  if (props.card.kind === "donut") {
-    return (props.card.donut || []).map((item) => ({
-      item: item.name,
-      count: item.value,
-    }));
+  if (props.card.kind === 'donut') {
+    return (props.card.donut || []).map((item) => ({ item: item.name, count: item.value }))
   }
 
   return (props.card.lines || []).flatMap((line) =>
     line.values.map((value, index) => ({
       item: props.card.xAxis?.[index] || String(index + 1),
       count: value,
-      group: line.name,
-    })),
-  );
-});
+      group: line.name
+    }))
+  )
+})
+
+const openSetting = () => {
+  settingVisible.value = true
+}
+
+const confirmSetting = () => {
+  settingVisible.value = false
+  window.$message?.success('参数设置成功')
+}
 
 const onDownload = async () => {
-  const success = await chartRef.value?.downloadImage(
-    props.card.title || "分析图",
-  );
+  const success = await chartRef.value?.downloadImage(props.card.title || '分析图')
   if (success) {
-    window.$message?.success("下载成功");
-    return;
+    window.$message?.success('下载成功')
+    return
   }
-  window.$message?.warning("当前图表暂不支持下载");
-};
+  window.$message?.warning('当前图表暂不支持下载')
+}
 </script>
 
 <style scoped lang="scss">
@@ -168,8 +159,8 @@ const onDownload = async () => {
   position: relative;
   display: flex;
   flex-direction: column;
-  min-height: 460px;
-  padding: 24px 26px;
+  min-height: 400px;
+  padding: 20px 24px;
   background: #fff;
   border-radius: 16px;
   box-shadow: rgba(222, 229, 250, 0.6) 1px 1px 9px 1px;
@@ -198,20 +189,20 @@ const onDownload = async () => {
   height: 20px;
   background: #337cff;
   border-radius: 2px;
-  content: "";
+  content: '';
 }
 
 .chart-card__body {
   flex: 1;
-  min-height: 350px;
-  margin-top: 18px;
+  min-height: 300px;
+  margin-top: 12px;
 }
 
 .chart-card__body--rank {
   display: grid;
-  grid-template-columns: minmax(0, 1fr) 280px;
+  grid-template-columns: minmax(360px, 1fr) 250px;
   align-items: center;
-  gap: 28px;
+  gap: 18px;
 }
 
 .chart-card__chart-wrap {
@@ -220,8 +211,8 @@ const onDownload = async () => {
 
 .chart-card__tools {
   position: absolute;
-  top: 22px;
-  right: 22px;
+  top: 20px;
+  right: 20px;
   z-index: 2;
   display: flex;
   flex-direction: column;
@@ -241,10 +232,7 @@ const onDownload = async () => {
   border: 1px solid #edf1f7;
   border-radius: 8px;
   cursor: pointer;
-  transition:
-    color 0.18s ease,
-    background-color 0.18s ease,
-    border-color 0.18s ease,
+  transition: color 0.18s ease, background-color 0.18s ease, border-color 0.18s ease,
     box-shadow 0.18s ease;
 }
 
@@ -259,17 +247,6 @@ const onDownload = async () => {
   background: #eef6ff;
   border-color: #b9d8ff;
   box-shadow: 0 4px 12px rgba(22, 119, 255, 0.14);
-}
-
-.chart-setting {
-  width: 180px;
-  padding: 4px;
-}
-
-.chart-setting__title {
-  margin-bottom: 12px;
-  color: #1f2329;
-  font-weight: 600;
 }
 
 .chart-rank {
@@ -320,6 +297,18 @@ const onDownload = async () => {
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
+}
+
+:global(.chart-setting-modal) {
+  width: 520px;
+}
+
+.chart-setting-form {
+  padding: 28px 32px 18px;
+}
+
+.chart-setting-input {
+  width: 100%;
 }
 
 @media (max-width: 1280px) {
